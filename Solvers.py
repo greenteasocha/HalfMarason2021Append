8d58cfdfddc95a11f6fdbe7205a5830f81671468
greenteasocha <kouheiatts@gmail.com>
2021-09-12 08:17:18 +0900

    収獲機を1個ではなくたくさん置くようにして、置く個数は適当に探索

--- Solvers.py
+++ Solvers.py
@@ -3,6 +3,7 @@ import re
 import random
 import math
 import copy
+import time
 from heapq import heappush, heappop, heapify
 from functools import cmp_to_key
 from bisect import bisect_left, bisect_right
@@ -28,6 +29,31 @@ getZList = lambda: [int(x) - 1 for x in input().split()]
 class BruteForceSolver(object):
     def __init__(self):
         self.simulator = FarmSimulator()
+        self.max_harvester = 0
+        self.operations_tmp = []
+
+    def iteration(self):
+        t_start = time.time()
+        max_score = 0
+        max_operations = []
+        for i in range(20, 40):
+            if time.time() - t_start > 1.7:
+                break
+            self.operations_tmp = []
+            self.max_harvester = i
+            self.simulator.reset()
+
+            self.solve()
+
+            # print("iteration: {}, score: {}".format(i, self.simulator.score))
+            if self.simulator.score > max_score:
+                max_score = self.simulator.score
+                max_operations = self.operations_tmp
+
+        for operation in max_operations:
+            print(*operation)
+
+        # print(max_score)
 
     def solve(self):
         for turn in range(self.simulator.t):
@@ -49,20 +75,17 @@ class BruteForceSolver(object):
                         max_v = cell_val
 
         if max_v > 0:
-            if lazy_x != -1:
-                self.op([lazy_y, lazy_x, max_y, max_x])
-                return
-            if simulator.score >= (simulator.num_harvesters + 1) ** 3:
+            # とりあえずn個くらい買う
+            able_to_buy = simulator.score >= (simulator.num_harvesters + 1) ** 3
+            if self.simulator.num_harvesters < self.max_harvester and able_to_buy:
                 self.op([max_y, max_x])
                 return
-                # if cell_val != 0 and not simulator.harvesters[y][x]:
-                #     if simulator.score >= (simulator.num_harvesters + 1) ** 3:
-                #         py, px = self.find_lazy_harvester()
-                #         if py == -1:
-                #             self.op([y, x])
-                #         else:
-                #             self.op([py, px, y, x])
-                #         return
+            elif lazy_x != -1:
+                self.op([lazy_y, lazy_x, max_y, max_x])
+                return
+            # if able_to_buy:
+            #     self.op([max_y, max_x])
+            #     return
 
         self.op([-1])
         return
@@ -77,5 +100,5 @@ class BruteForceSolver(object):
         return [-1, -1]
 
     def op(self, args):
-        print(*args)
+        self.operations_tmp.append(args)
         self.simulator.step(args)
